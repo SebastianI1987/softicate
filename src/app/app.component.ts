5076b24f06d35ccd70348fb1a625bbfee3a56630
Sebastian Ibe <dasmischwerk@gmail.com>
2023-08-08 23:31:42 +0200

    Bugfix of chosen object and label

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, AfterViewInit, ElementRef } from '@angular/core';
 import { Node } from './node.model';
-import { Network } from 'vis-network/standalone/esm/vis-network';
+import { Network, Options } from 'vis-network/standalone/esm/vis-network';
 import { DataSet } from 'vis-data/peer/esm/vis-data';
+import { configureOptions } from 'vis-network/declarations/network/options';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +15,16 @@ export class AppComponent implements AfterViewInit {
   value: number = 0;
   nodeName: string = '';
   connectedNodes: string = '';
+  network: any;
+  mouseTarget: any;
 
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef) {
+
+
+  }
 
   ngAfterViewInit(): void {
+
     // Create initial nodes
     const node1 = new Node(10, 'Node 1');
     const node2 = new Node(20, 'Node 2');
@@ -36,13 +43,37 @@ export class AppComponent implements AfterViewInit {
     ]);
 
     // Create a network graph
+    
     const container = this.elementRef.nativeElement.querySelector('#mynetwork');
     const data = {
       nodes: this.nodes,
       edges: this.edges
     };
-    const options = {};
+    const options = {
+      configure:{   enabled: true,
+        filter: 'nodes,edges',
+        container: undefined,
+        showButton: true},
+      nodes: {
+        shape: 'box',
+        borderRadius: 8,
+        borderWidth: 1,
+        borderWidthSelected: 2,
+        chosen: {node: (values:any, id:any, selected:any, hovering:any) => {
+          values.borderWidth = 2;
+          values.borderColor = '#E81E6E';
+        },
+        label: false},
+        },
+      edges: {arrows: {
+        to: {enabled: true, scaleFactor: 1, type: 'arrow'}, 
+        from: {enabled: true, scaleFactor: 1, type: 'circle'}}},
+      };
     const network = new Network(container, data, options);
+
+    this.mouseTarget = this.elementRef.nativeElement.querySelector('#mouseTarget');
+    this.mouseTarget.addEventListener('mouseenter', (event:any) => {this.mouseTarget.style.backgroundColor = 'red'});
+    this.mouseTarget.addEventListener('mouseleave', (event:any) => {this.mouseTarget.style.backgroundColor = 'blue'});
   }
 
   addNode(): void {
@@ -60,5 +91,11 @@ export class AppComponent implements AfterViewInit {
         this.edges.add({ from: connectedNode.id, to: newNode.value });
       }
     });
+    alert(this.network.getSelectedNodes())
   }
+
+  
+
+
 }
+
